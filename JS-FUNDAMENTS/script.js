30954cc145eeac278bd27611fcd00a7427b427d5
Ruxandra Laura <Berbecutza1986@gmail.com>
2022-08-23 17:54:33 +0300

    JS-Fundaments.

--- JS-FUNDAMENTS/script.js
+++ JS-FUNDAMENTS/script.js
@@ -1,5 +1,5 @@
 let message = "Hello";
-alert(message);
+// alert(message);
 //  O modalitate de a declara variabilele
 // let user = "John",
 //   age = 20,
@@ -9,3 +9,43 @@ alert(message);
 let user = "John";
 let age = 20;
 let Message = "Hey";
+// We can change the value of variable:
+user = "Smith";
+age = 25;
+Message = "Hey World!";
+console.log(user, age, Message);
+// Declare 2 variables and copy data from one into the other
+let hello = "Hello World!";
+let MEssage;
+MEssage = hello;
+
+console.log(hello); // Hello World!
+console.log(MEssage); // Hello World!
+// What can and can't be use to declare name of variable
+// Can use $, _, camelCase, PascaleCase,
+// Nu se poate incepe o variabila cu numere 3camel-NU, 12John-NU,
+// Nu se poate folosi(-)my-Name-NU deoarece (-) Semnul Scaderii considerat
+let $ = 5;
+let _ = 9;
+console.log($ + _); // 14
+//JAVASCRIPT II CASE_SENSITIVE  NU= case_sensitive
+// Numele variabileleo scris in Engleza
+
+// Const e folosita atunci cand stim sigur ca valoarea nu se schimba
+const myBirthday = "02.12.1986";
+// Numele Variabilelor ar trebui sa fie curate si sa insemne informatia stocata
+const firstName = "Ruxandra";
+// TASK-EXERCISES
+// 01
+let admin;
+let NAMe = "Joohnny";
+admin = NAMe;
+console.log(admin);
+// alert(admin); //II BINE FACUT
+// 02
+const planetWhereBreatheOxigen = "Earth";
+let newUser = "Nicoleta Diana";
+// console.log(newUser);
+// uppercase pt VARIABILE se foloseste pt const care sunt "hard-coded"
+//adica atunci cand stii valoarea ex: const BIRTHDAY = "02.12.1986" si
+//direct scrisa in cod.
