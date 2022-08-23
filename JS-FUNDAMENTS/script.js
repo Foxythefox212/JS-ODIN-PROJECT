5297aac451d5703c541bb81bb1339b623e0e1d5d
Ruxandra Laura <Berbecutza1986@gmail.com>
2022-08-23 17:08:04 +0300

    JavaScript-the Odin Project

---
+++ JS-FUNDAMENTS/script.js
@@ -0,0 +1,11 @@
+let message = "Hello";
+alert(message);
+//  O modalitate de a declara variabilele
+// let user = "John",
+//   age = 20,
+//   Message = "Hey";
+
+// Modalitatea cea mai indicata
+let user = "John";
+let age = 20;
+let Message = "Hey";
