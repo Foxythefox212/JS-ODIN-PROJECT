3ac370427a006153a86d76b15642357c02e44903
Ruxandra Laura <Berbecutza1986@gmail.com>
2022-08-24 17:07:44 +0300

    Exercitii de verificare.

---
+++ JS-FUNDAMENTS/assignment-lesson.js
@@ -0,0 +1,10 @@
+let a = 10;
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+console.log(23 + 5);
+console.log(1, 2, 3, 4, 5, 6);
+console.log((4 + 6 + 9) / 77);
+const max = 57;
+let actual = max - 13;
+let percentage = actual / max;
